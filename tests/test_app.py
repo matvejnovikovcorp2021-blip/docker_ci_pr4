0a660e3a195786223ac7c5c36bfcd708ae1bd822
matvejnovikovcorp2021-blip <matvejnovikov.corp.2021@gmail.com>
2026-05-05 22:21:32 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 from app import app
 
@@ -5,6 +9,7 @@ from app import app
 def client():
     with app.test_client() as client:
         yield client
+
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
