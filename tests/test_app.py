90a94ef8d2f6dfb94c48bd8330a11ce1b4e67be5
Matvey <mateo_novi@mail.ru>
2026-05-05 18:41:15 +0300

    Change test filee

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,6 @@ from app import app
 def client():
     with app.test_client() as client:
         yield client
-
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
